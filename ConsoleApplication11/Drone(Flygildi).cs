bb7179175af59be34b4058eb637d57a3e6159308
Þorgeir <thorgeir@radiantgames.is>
2016-07-15 00:34:13 +0000

    First commit of the space ship

--- ConsoleApplication11/Drone(Flygildi).cs
+++ ConsoleApplication11/Drone(Flygildi).cs
@@ -1,101 +1,101 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
 
-public struct Point2D
-{
-	public int X; public int Y;
-	public override string ToString() { return X + " " + Y; }
-	internal double DistanceSquaredTo(Point2D p)
-	{
-		return Math.Pow(X - p.X, 2) + Math.Pow(Y - p.Y, 2);
-	}
-}
+//public struct Point2d
+//{
+//	public int X; public int Y;
+//	public override string ToString() { return X + " " + Y; }
+//	internal double DistanceSquaredTo(Point2d p)
+//	{
+//		return Math.Pow(X - p.X, 2) + Math.Pow(Y - p.Y, 2);
+//	}
+//}
 
-class Program
-{
-	static Point2D[] Points;
-	static int N;
-	static void Main()
-	{
-		N = int.Parse(Console.ReadLine());
-		Points = GetPoints(N);
-		var cur = new Point2D();
-		var done = new HashSet<Point2D>();
-		double alwaysClosest = 0;
+//class Program
+//{
+//	static Point2d[] Points;
+//	static int N;
+//	static void Main()
+//	{
+//		N = int.Parse(Console.ReadLine());
+//		Points = GetPoints(N);
+//		var cur = new Point2d();
+//		var done = new HashSet<Point2d>();
+//		double alwaysClosest = 0;
 
-		if (Points.All(i => i.X == 0))
-		{
-			var max = Points.Max(i => i.Y);
-			var min = Points.Min(i => i.Y);
-			double dist = 0;
-			if (max >= 0 && min <= 0)
-				dist = Math.Sqrt(max * max) * 2 + Math.Sqrt(min * min) * 2;
-			else if (max > 0 && min > 0)
-				dist = Math.Sqrt(max * max) * 2;
-			else if (max < 0 && min < 0)
-				dist = Math.Sqrt(min * min) * 2;
+//		if (Points.All(i => i.X == 0))
+//		{
+//			var max = Points.Max(i => i.Y);
+//			var min = Points.Min(i => i.Y);
+//			double dist = 0;
+//			if (max >= 0 && min <= 0)
+//				dist = Math.Sqrt(max * max) * 2 + Math.Sqrt(min * min) * 2;
+//			else if (max > 0 && min > 0)
+//				dist = Math.Sqrt(max * max) * 2;
+//			else if (max < 0 && min < 0)
+//				dist = Math.Sqrt(min * min) * 2;
 
-			Console.WriteLine(dist);
-			return;
-		}
-		for (int i = 0; i < N; i++)
-		{
-			var next = Closest(cur, done);
-			done.Add(next);
-			alwaysClosest += Math.Sqrt(cur.DistanceSquaredTo(next));
-			cur = next;
-		}
-		alwaysClosest += Math.Sqrt(cur.X * cur.X + cur.Y * cur.Y);
+//			Console.WriteLine(dist);
+//			return;
+//		}
+//		for (int i = 0; i < N; i++)
+//		{
+//			var next = Closest(cur, done);
+//			done.Add(next);
+//			alwaysClosest += Math.Sqrt(cur.DistanceSquaredTo(next));
+//			cur = next;
+//		}
+//		alwaysClosest += Math.Sqrt(cur.X * cur.X + cur.Y * cur.Y);
 
 
 
-		var sum = DFS(new Point2D(), 0, alwaysClosest, new HashSet<Point2D>());
-		Console.WriteLine(sum);
-	}
+//		var sum = DFS(new Point2d(), 0, alwaysClosest, new HashSet<Point2d>());
+//		Console.WriteLine(sum);
+//	}
 
-	static double DFS(Point2D cur, double curDist, double bestDist, HashSet<Point2D> done)
+//	static double DFS(Point2d cur, double curDist, double bestDist, HashSet<Point2d> done)
 
-	{
-		if (curDist > bestDist) return bestDist;
-		if (done.Count == Points.Length)
-		{
-			curDist += Math.Sqrt(cur.X * cur.X + cur.Y * cur.Y);
-			if (curDist > bestDist) return bestDist;
-			else return curDist;
-		}
-		for (int i = 0; i < N; i++)
-		{
-			var p = Points[i];
-			if (done.Contains(p)) continue;
-			var newDist = curDist + Math.Sqrt(cur.DistanceSquaredTo(p));
-			done.Add(p);
-			bestDist = DFS(p, newDist, bestDist, done);
-			done.Remove(p);
-		}
-		return bestDist;
-	}
+//	{
+//		if (curDist > bestDist) return bestDist;
+//		if (done.Count == Points.Length)
+//		{
+//			curDist += Math.Sqrt(cur.X * cur.X + cur.Y * cur.Y);
+//			if (curDist > bestDist) return bestDist;
+//			else return curDist;
+//		}
+//		for (int i = 0; i < N; i++)
+//		{
+//			var p = Points[i];
+//			if (done.Contains(p)) continue;
+//			var newDist = curDist + Math.Sqrt(cur.DistanceSquaredTo(p));
+//			done.Add(p);
+//			bestDist = DFS(p, newDist, bestDist, done);
+//			done.Remove(p);
+//		}
+//		return bestDist;
+//	}
 
-	static Point2D Closest(Point2D p, HashSet<Point2D> done)
-	{
-		return (from point in Points
-				where !done.Contains(point)
-				orderby Math.Pow(p.X - point.X, 2) + Math.Pow(p.Y - point.Y, 2)
-				select point).First();
-	}
+//	static Point2d Closest(Point2d p, HashSet<Point2d> done)
+//	{
+//		return (from point in Points
+//				where !done.Contains(point)
+//				orderby Math.Pow(p.X - point.X, 2) + Math.Pow(p.Y - point.Y, 2)
+//				select point).First();
+//	}
 
-	private static Point2D[] GetPoints(int N)
-	{
-		var points = new Point2D[N];
-		for (int i = 0; i < N; i++)
-		{
-			var data = Console.ReadLine().Split(' ');
-			points[i] = new Point2D
-			{
-				X = int.Parse(data[0]),
-				Y = int.Parse(data[1])
-			};
-		}
-		return points;
-	}
-}
+//	private static Point2d[] GetPoints(int N)
+//	{
+//		var points = new Point2d[N];
+//		for (int i = 0; i < N; i++)
+//		{
+//			var data = Console.ReadLine().Split(' ');
+//			points[i] = new Point2d
+//			{
+//				X = int.Parse(data[0]),
+//				Y = int.Parse(data[1])
+//			};
+//		}
+//		return points;
+//	}
+//}
---
+++ ConsoleApplication11/Geimskip.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public struct Sphere
+{
+	public int X; public int Y; public int Z; public int R;
+	public override string ToString() { return string.Format("({0},{1},{2}) R {3}", X, Y, Z, R); }
+	public bool Collides(Sphere other)
+	{
+		var MinRange = R + other.R;
+		var Distance = Math.Sqrt(
+			Math.Pow(X - other.X, 2) +
+			Math.Pow(Y - other.Y, 2) +
+			Math.Pow(Z - other.Z, 2));
+		return MinRange >= Distance;
+	}
+}
+
+class Program
+{
+	static Sphere[] Ships;
+	static Sphere[] Bombs;
+	static int N;
+	static int M;
+	static void Main()
+	{
+		N = int.Parse(Console.ReadLine());
+		Ships = GetSpheres(N);
+		M = int.Parse(Console.ReadLine());
+		Bombs = GetSpheres(M);
+
+		var destroyed = new HashSet<int>();
+		var newBombs = new Queue<Sphere>();
+		for (int i = 0; i < N; i++)
+		{
+			var ship = Ships[i];
+			for (int j = 0; j < M; j++)
+			{
+				var bomb = Bombs[j];
+				if (ship.Collides(bomb))
+				{
+					destroyed.Add(i);
+					ship.R *= 2;
+					newBombs.Enqueue(ship);
+					break;
+				}
+			}
+		}
+		while (newBombs.Count > 0)
+		{
+			var bomb = newBombs.Dequeue();
+			for (int i = 0; i < N; i++)
+			{
+				if (destroyed.Contains(i)) continue;
+				var ship = Ships[i];
+				if (ship.Collides(bomb))
+				{
+					destroyed.Add(i);
+					ship.R *= 2;
+					newBombs.Enqueue(ship);
+					break;
+				}
+			}
+		}
+
+		Console.WriteLine(N - destroyed.Count);
+	}
+
+	private static Sphere[] GetSpheres(int N)
+	{
+		var spheres = new Sphere[N];
+		for (int i = 0; i < N; i++)
+		{
+			var data = Console.ReadLine().Split(' ');
+			spheres[i] = new Sphere
+			{
+				X = int.Parse(data[0]),
+				Y = int.Parse(data[1]),
+				Z = int.Parse(data[2]),
+				R = int.Parse(data[3])
+			};
+		}
+		return spheres;
+	}
+}
