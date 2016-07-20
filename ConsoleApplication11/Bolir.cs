7d03cb43b079cbad7ea4c65abfd9230acef528ae
Þorgeir <thorgeir@radiantgames.is>
2016-07-20 16:10:33 +0000

    Improved the preprocessing of the Shirts.

--- ConsoleApplication11/Bolir.cs
+++ ConsoleApplication11/Bolir.cs
@@ -19,8 +19,11 @@ class Program
 		var shirtsRequests = new ShirtRequest[N];
 		var shirts = new Dictionary<int, int>();
 
-		var max = ReadShirtRequest(shirtsRequests);
-		ReadShirts(N, shirts, max);
+		//var max = ReadShirtRequestRand(shirtsRequests);
+		//ReadShirtsRand(N, shirts, max);
+
+		ReadShirtRequest(shirtsRequests);
+		ReadShirts(N, shirts);
 
 		var done = new HashSet<int>();
 		if (!Preprocess(N, shirtsRequests, shirts, done))
@@ -63,43 +66,106 @@ class Program
 				}
 			}
 			done.Remove(i);
+			return false;
 		}
 		return false;
 	}
 
 	private static bool Preprocess(int N, ShirtRequest[] shirtsRequests, Dictionary<int, int> shirts, HashSet<int> done)
 	{
-		for (int i = 0; i < N; i++)
+		bool needToCheckAgain;
+		do
 		{
-			var req = shirtsRequests[i];
-			if (req.Min == req.Max)
+			needToCheckAgain = false;
+			for (int i = 0; i < N; i++)
 			{
-				if (shirts.ContainsKey(req.Min))
+				if (done.Contains(i)) continue;
+
+				var req = shirtsRequests[i];
+				if (req.Min == req.Max)
 				{
-					done.Add(i);
-					shirts[req.Min] -= 1;
-					if (shirts[req.Min] == 0) shirts.Remove(req.Min);
+					if (shirts.ContainsKey(req.Min))
+					{
+						done.Add(i);
+						shirts[req.Min] -= 1;
+						if (shirts[req.Min] == 0)
+						{
+							needToCheckAgain = true;
+							shirts.Remove(req.Min);
+						}
+					}
+					else
+					{
+						return false;
+					}
 				}
 				else
 				{
-					return false;
+					var s = shirts.Keys.Where(shirt => shirt >= req.Min && shirt <= req.Max);
+					var count = s.Count();
+					if (count == 0)
+					{
+						return false;
+					}
+					if (count == 1)
+					{
+						needToCheckAgain = true;
+						done.Add(i);
+						var size = s.First();
+						shirts[size] -= 1;
+						if (shirts[size] == 0) shirts.Remove(size);
+					}
 				}
 			}
-			else if (!shirts.Keys.Any(shirt => shirt >= req.Min && shirt <= req.Max))
+
+			if (!needToCheckAgain)
 			{
-				return false;
+				var modifications = new Dictionary<int, int>();
+				foreach (var s in shirts)
+				{
+					var reqIndex = -1;
+					var found = true;
+					for (int i = 0; i < N; i++)
+					{
+						if (done.Contains(i)) continue;
+
+						var sReq = shirtsRequests[i];
+						if (sReq.Min <= s.Key && sReq.Max >= s.Key)
+						{
+							if (reqIndex > -1)
+							{
+								found = false;
+								break;
+							}
+							reqIndex = i;
+						}
+					}
+					if (found)
+					{
+						done.Add(reqIndex);
+						if (modifications.ContainsKey(s.Key))
+							modifications[s.Key] += 1;
+						else
+							modifications[s.Key] = 1;
+						needToCheckAgain = true;
+					}
+				}
+				foreach (var mod in modifications)
+				{
+					shirts[mod.Key] -= mod.Value;
+					if (shirts[mod.Key] <= 0) shirts.Remove(mod.Key);
+				}
 			}
-		}
+		} while (needToCheckAgain);
 		return true;
 	}
 
-	private static void ReadShirts(int N, Dictionary<int, int> shirts, int max)
+	private static void ReadShirts(int N, Dictionary<int, int> shirts)
 	{
-		var rand = new Random();
-		//var input = Console.ReadLine().Split(' ');
+		var input = Console.ReadLine().Split(' ');
 		for (int i = 0; i < N; i++)
 		{
-			var key = rand.Next(1, max + 1); //int.Parse(input[i]);
+			var key = int.Parse(input[i]);
 			if (shirts.ContainsKey(key))
 				shirts[key] += 1;
 			else
@@ -107,24 +173,47 @@ class Program
 		}
 	}
 
-	private static int ReadShirtRequest(ShirtRequest[] shirts)
+	private static void ReadShirtRequest(ShirtRequest[] shirts)
+	{
+		for (int i = 0; i < shirts.Length; i++)
+		{
+			var input = Console.ReadLine().Split(' ');
+			shirts[i] = new ShirtRequest
+			{
+				Min = int.Parse(input[0]),
+				Max = int.Parse(input[1])
+			};
+		}
+	}
+
+
+	private static int ReadShirtRequestRand(ShirtRequest[] shirts)
 	{
 		var rand = new Random();
 		var totalMax = 0;
 		for (int i = 0; i < shirts.Length; i++)
 		{
-			//var input = Console.ReadLine().Split(' ');
-			var min = rand.Next(1, 1001);
-			var max = rand.Next(min, 1001);
+			var min = rand.Next(1, 101);
+			var max = rand.Next(min, 101);
 			if (totalMax < max) totalMax = max;
 			shirts[i] = new ShirtRequest
 			{
 				Min = min,
 				Max = max
-				//Min = int.Parse(input[0]),
-				//Max = int.Parse(input[1])
 			};
 		}
 		return totalMax;
+	}
+	private static void ReadShirtsRand(int N, Dictionary<int, int> shirts, int max)
+	{
+		var rand = new Random();
+		for (int i = 0; i < N; i++)
+		{
+			var key = rand.Next(1, max + 1);
+			if (shirts.ContainsKey(key))
+				shirts[key] += 1;
+			else
+				shirts[key] = 1;
+		}
 	}
 }
