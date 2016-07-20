10cc58ba9276d206aee9c5cd3be59144bdd87d6a
Þorgeir <thorgeir@radiantgames.is>
2016-07-20 01:39:17 +0000

    Geimskip Buið Symmetry 30/100 Bolir að byrja

--- ConsoleApplication11/Geimskip.cs
+++ ConsoleApplication11/Geimskip.cs
@@ -1,86 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿//using System;
+//using System.Collections.Generic;
 
-public struct Sphere
-{
-	public int X; public int Y; public int Z; public int R;
-	public override string ToString() { return string.Format("({0},{1},{2}) R {3}", X, Y, Z, R); }
-	public bool Collides(Sphere other)
-	{
-		var MinRange = R + other.R;
-		var Distance = Math.Sqrt(
-			Math.Pow(X - other.X, 2) +
-			Math.Pow(Y - other.Y, 2) +
-			Math.Pow(Z - other.Z, 2));
-		return MinRange >= Distance;
-	}
-}
+//public struct Sphere
+//{
+//	public int X; public int Y; public int Z; public int R;
+//	public override string ToString() { return string.Format("({0},{1},{2}) R {3}", X, Y, Z, R); }
+//	public bool Collides(Sphere other)
+//	{
+//		var MinRange = R + other.R;
+//		var Distance = Math.Sqrt(
+//			Math.Pow(X - other.X, 2) +
+//			Math.Pow(Y - other.Y, 2) +
+//			Math.Pow(Z - other.Z, 2));
+//		return MinRange >= Distance;
+//	}
+//}
+//class Program
+//{
+//	static Sphere[] Ships;
+//	static Sphere[] Bombs;
+//	static void Main()
+//	{
+//		var N = int.Parse(Console.ReadLine());
+//		Ships = GetSpheres(N);
+//		var M = int.Parse(Console.ReadLine());
+//		Bombs = GetSpheres(M);
 
-class Program
-{
-	static Sphere[] Ships;
-	static Sphere[] Bombs;
-	static int N;
-	static int M;
-	static void Main()
-	{
-		N = int.Parse(Console.ReadLine());
-		Ships = GetSpheres(N);
-		M = int.Parse(Console.ReadLine());
-		Bombs = GetSpheres(M);
+//		var destroyed = new HashSet<int>();
+//		var newBombs = new Queue<Sphere>();
+//		for (int i = 0; i < N; i++)
+//		{
+//			var ship = Ships[i];
+//			for (int j = 0; j < M; j++)
+//			{
+//				if (ship.Collides(Bombs[j]))
+//				{
+//					destroyed.Add(i);
+//					ship.R *= 2;
+//					newBombs.Enqueue(ship);
+//					break;
+//				}
+//			}
+//		}
+//		while (newBombs.Count > 0)
+//		{
+//			var bomb = newBombs.Dequeue();
+//			for (int i = 0; i < N; i++)
+//			{
+//				if (destroyed.Contains(i)) continue;
+//				var ship = Ships[i];
+//				if (bomb.Collides(ship))
+//				{
+//					destroyed.Add(i);
+//					ship.R *= 2;
+//					newBombs.Enqueue(ship);
+//				}
+//			}
+//		}
 
-		var destroyed = new HashSet<int>();
-		var newBombs = new Queue<Sphere>();
-		for (int i = 0; i < N; i++)
-		{
-			var ship = Ships[i];
-			for (int j = 0; j < M; j++)
-			{
-				var bomb = Bombs[j];
-				if (ship.Collides(bomb))
-				{
-					destroyed.Add(i);
-					ship.R *= 2;
-					newBombs.Enqueue(ship);
-					break;
-				}
-			}
-		}
-		while (newBombs.Count > 0)
-		{
-			var bomb = newBombs.Dequeue();
-			for (int i = 0; i < N; i++)
-			{
-				if (destroyed.Contains(i)) continue;
-				var ship = Ships[i];
-				if (ship.Collides(bomb))
-				{
-					destroyed.Add(i);
-					ship.R *= 2;
-					newBombs.Enqueue(ship);
-					break;
-				}
-			}
-		}
+//		Console.WriteLine(N - destroyed.Count);
+//	}
 
-		Console.WriteLine(N - destroyed.Count);
-	}
-
-	private static Sphere[] GetSpheres(int N)
-	{
-		var spheres = new Sphere[N];
-		for (int i = 0; i < N; i++)
-		{
-			var data = Console.ReadLine().Split(' ');
-			spheres[i] = new Sphere
-			{
-				X = int.Parse(data[0]),
-				Y = int.Parse(data[1]),
-				Z = int.Parse(data[2]),
-				R = int.Parse(data[3])
-			};
-		}
-		return spheres;
-	}
-}
+//	private static Sphere[] GetSpheres(int N)
+//	{
+//		//var rand = new Random();
+//		var points = new Sphere[N];
+//		for (int i = 0; i < N; i++)
+//		{
+//			var data = Console.ReadLine().Split(' ');
+//			points[i] = new Sphere
+//			{
+//				X = /*rand.Next(-10000, 10001),*/ int.Parse(data[0]),
+//				Y = /*rand.Next(-10000, 10001),*/ int.Parse(data[1]),
+//				Z = /*rand.Next(-10000, 10001),*/ int.Parse(data[2]),
+//				R = /*rand.Next(1, 1000),*/ int.Parse(data[3])
+//			};
+//		}
+//		return points;
+//	}
+//}
---
+++ ConsoleApplication11/Symmetry.cs
@@ -0,0 +1,35 @@
+﻿//using System;
+
+//class Program
+//{
+//	static void Main()
+//	{
+//		for (int i = 1; i <= 100; i++)
+//		{
+//			for (var j = Math.Pow(2, i); j > 0; j--)
+//			{
+//				// 35184372088832
+//				// 35184366348153
+//				if (IsSymmetry(j))
+//				{
+//					Console.WriteLine(i + " " + j);
+//					break;
+//				}
+//			}
+//		}
+//	}
+
+//	private static bool IsSymmetry(double symmetry)
+//	{
+//		var str = symmetry.ToString();
+//		var strLen = str.Length;
+//		for (int i = 0; i < strLen / 2; i++)
+//		{
+//			if (str[i] != str[strLen - i - 1])
+//			{
+//				return false;
+//			}
+//		}
+//		return true;
+//	}
+//}
